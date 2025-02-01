3ea8c15a28b7ae4a1ceb4e7da66da06e56cf72ab
Rahul Sethuram <rahul.eth@icloud.com>
2025-02-01 09:55:29 +0400

    fux

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { Reflector } from '@nestjs/core';
 
 async function bootstrap() {
   // @ts-expect-error BigInt.prototype.toJSON is not defined
@@ -25,7 +26,7 @@ async function bootstrap() {
   );
 
   // Enable logging interceptor globally
-  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new LoggingInterceptor(new Reflector()));
 
   // Configure Swagger
   const config = new DocumentBuilder()
